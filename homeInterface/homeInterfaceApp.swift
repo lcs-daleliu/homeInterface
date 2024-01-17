f7762a1b92d5627cfa027d4c5e6206893910c01f
Dale Liu <YLiu@lcs.on.ca>
2024-01-16 19:05:20 -0500

    Initial Commit

---
+++ homeInterface/homeInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  homeInterfaceApp.swift
+//  homeInterface
+//
+//  Created by Yiren LIU on 2024-01-16.
+//
+
+import SwiftUI
+
+@main
+struct homeInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
